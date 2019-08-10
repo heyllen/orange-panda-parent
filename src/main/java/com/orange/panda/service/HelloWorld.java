5abe0dc2cceb2eaa51d7b00b020f490c4c51ec55
heyllen <393286997@qq.com>
2019-08-11 03:12:30 +0800

    提交版本库 第二次

---
+++ src/main/java/com/orange/panda/service/HelloWorld.java
@@ -0,0 +1,25 @@
+package com.orange.panda.service;
+
+/**
+ * Created by Administrator on 2019/8/11.
+ */
+public class HelloWorld {
+
+    public static void main(String args[]) {
+        System.out.print(11111111);
+        int i = 100;
+        for (i = 0; i < 10; i++) {
+
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorld{}";
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
